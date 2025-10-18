31b10da32d2b449cd12b96fb608d900e924e026f
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-18 14:53:28 +0530

    first commit

---
+++ frontend.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-roboshop"
+FILE_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
+MONGODB_HOST=mongodb.nkrdev.space
+SCRIPT_DIR=$PWD
+mkdir -p $LOG_FOLDER
+
+echo "script started and executed at :  $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Error :: please run the script by using root user $N "
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error :: Failed to $2 server $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 server $N" | tee -a $LOG_FILE
+fi
+}
+
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+
+rm -rf /etc/nginx/nginx.conf 
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Coping nginx"
+
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting nginx"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
