4f29ea1544df88c1e80e76ece42cfb8efb49b7aa
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-18 12:05:56 +0530

    first commit

---
+++ catalogue.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+app_setup
+Nodejs
+systemd_setup
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+VALIDATE $? "Adding mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "installing the mongodb"
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "load catalogue products"
+
+app_restart
+print_total_time
--- common.sh
+++ common.sh
@@ -9,6 +9,8 @@ LOG_FOLDER="/var/log/shell-roboshop"
 FILE_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.nkrdev.space
 mkdir -p $LOG_FOLDER
 
 echo "script started and executed at :  $(date)" | tee -a $LOG_FILE
@@ -28,7 +30,49 @@ VALIDATE(){
     fi
 }
 
+Nodejs(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disable nodejs"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enable nodejs"
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing nodejs"
 
+    npm install  &>>$LOG_FILE
+    VALIDATE $? "installing the library"
+}
+
+app_setup(){
+    mkdir -p /app 
+    VALIDATE $? "creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "downloading $app_name application"
+
+    cd /app 
+    VALIDATE $? "changing to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "removing exissting code"
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzip the file"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service 
+    VALIDATE $? "Adding $app_name repo"
+
+    systemctl daemon-reload
+    VALIDATE $? "Reload the file"
+
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "enable the $app_name"
+}
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarting $app_name"
+}
 print_total_time(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(($END_TIME-$START_TIME))
